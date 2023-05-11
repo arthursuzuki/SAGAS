0ff24970618b31342a746f00bf34f35e367e8f57
Arthur Suzuki <126833330+arthursuzuki@users.noreply.github.com>
2023-05-10 23:05:33 -0300

    Cubeto com entrada de comandos
    Nesse código podemos definir manualmente qual vai ser a função utilizada.
    
    In this code, we can define manually the function that's going to be used.

---
+++ Cubeto-Entrada-Comandos.ino
@@ -0,0 +1,83 @@
+const int led = 13;
+const int botao1 = 12;
+const int botao2 = 11;
+
+bool estadoLed = LOW;
+bool botao2Pressionado = false;
+
+void Fio(){
+  digitalWrite(led, LOW);
+  while (digitalRead(botao1) == LOW){
+    digitalWrite(led, HIGH);
+  }
+}
+
+void Not(){
+  digitalWrite(led, HIGH);
+  while (digitalRead(botao1) == LOW){
+    digitalWrite(led, LOW);
+  }
+}
+
+void Or(){
+  digitalWrite(led, LOW);
+  while ((digitalRead(botao1) == LOW) || (digitalRead(botao2) == LOW)){
+    digitalWrite(led, HIGH);
+  }
+}
+
+void And(){
+  digitalWrite(led, LOW);
+  while ((digitalRead(botao1) == LOW) && (digitalRead(botao2) == LOW)){
+    digitalWrite(led, HIGH);
+  }
+}
+
+void Xor(){
+  digitalWrite(led, LOW);
+  while (((digitalRead(botao1) == LOW) || (digitalRead(botao2) == LOW)) && (digitalRead(botao1) != digitalRead(botao2))){
+    delay(50);
+    digitalWrite(led, HIGH);
+  }
+}
+
+void Latch(){
+  digitalWrite(led, estadoLed);
+
+  if ((digitalRead(botao2) == LOW) && (botao2Pressionado == false)){
+    botao2Pressionado = true;
+    estadoLed = HIGH;
+  }
+  if ((digitalRead(botao1) == LOW) && (botao2Pressionado == true)){
+    botao2Pressionado = false;
+    estadoLed = LOW;
+  }
+}
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+  pinMode(botao1, INPUT_PULLUP);
+  pinMode(botao2, INPUT_PULLUP);
+}
+
+void loop(){
+  if (Serial.available()){
+    String data = Serial.readString();
+    if (data==("Not")){
+      Not();
+    }
+    if (data==("Or")){
+      Or();
+    }
+    if (data==("And")){
+      And();
+    }
+    if (data==("Xor")){
+      Xor();
+    }
+    if(data==("Latch")){
+      Latch();
+    }
+  }
+}
